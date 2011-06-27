927673e1a75c9b7d68f7da3f3949b832c78b67e6
Michal Staniaszek <m.staniaszek@gmail.com>
2011-06-27 18:57:47 +0100

    finished q1 chapter 2

---
+++ 2-chap/1q.hs
@@ -0,0 +1,10 @@
+let area r = r * r in area 1.532  -- a 
+
+let avg [a,b,c,d] = (a+b+c+d)/4 in avg [1.23, 2.45, -6.50, 9.76] -- b
+
+let a = 1.2; b = 3.4; c = 5.6; p = 7.8; q = 9.0; x = p - 2 * q; in a + b * x + c * x * x -- c
+
+let s_area r h = pi * r * r * h where pi = 3.14159263 in s_area 5.678 1.234 -- d
+
+s_area 9.765 0.864
+s_area 5.371 9.364
