da411de0bd6ef44564e97a05d6942ffa60a81ba6
Michal Staniaszek <m.staniaszek@gmail.com>
2011-06-28 22:23:51 +0100

    Started on exercise 1

---
+++ 1.hs
@@ -0,0 +1,40 @@
+mnmInt :: [Int] -> Int
+mnmInt [] = error "Empty list"
+mnmInt [x] = x
+mnmInt (x:xs) = min x (mnmInt xs)
+
+-- 1.9
+maxInt :: [Int] -> Int
+maxInt [] = error "Empty list"
+maxInt [x] = x
+maxInt (x:xs) = max x (maxInt xs)
+
+max' x y | x >= y = x
+         | otherwise = y
+                       
+-- 1.10
+removeFst :: Int -> [Int] -> [Int]
+removeFst m [] = error "Empty list"
+removeFst m [x] | m == x = []
+                | otherwise = [x]
+removeFst m (x:xs) | m == x = xs
+                   | otherwise = x : removeFst m xs
+
+--
+srtInts :: [Int] -> [Int]
+srtInts [] = []
+srtInts xs = m : (srtInts (removeFst m xs)) where m = mnmInt xs
+                                                  
+srtInts' :: [Int] -> [Int]
+srtInts' [] = []
+srtInts' xs = let 
+  m = mnmInt xs 
+  in m : (srtInts' (removeFst m xs))
+     
+average :: [Int] -> Float
+average [] = error "Empty list"
+average xs = fromInt (sum xs) / fromInt (length xs)
+
+sum' :: [Int] -> Int
+sum' [] = error "Empty list"
+sum' (x:xs) = x + sum' xs
