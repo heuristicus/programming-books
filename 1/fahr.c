0ff04c7d536c29d7c58d11b2e5a86d50da4c7b79
Michal Staniaszek <m.staniaszek@gmail.com>
2011-08-03 18:33:23 +0100

    ex 1-4. celsius program

--- 1/fahr.c
+++ 1/fahr.c
@@ -11,7 +11,7 @@ main() {
 
   fahr = lower;
   printf("%10s %7s\n", "Fahrenheit", "Celsius");
-  while(fahr<=upper){
+  while(fahr <= upper){
       celsius = (5.0/9.0) * (fahr-32.0);
       printf("%10.0f %7.1f\n", fahr, celsius);
       fahr = fahr + step;
