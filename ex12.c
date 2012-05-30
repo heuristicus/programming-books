2311be4eb0a3d30e48da0ebf79430f1745c2224c
Michal Staniaszek <m.staniaszek@gmail.com>
2012-05-30 13:27:02 +0900

    12 updated

--- ex12.c
+++ ex12.c
@@ -4,7 +4,9 @@ int main(int argc, char *argv[])
 {
   int i = 0;
 
-  if (argc == 2){
+  if (argc == 1){
+    printf("You've not entered any arguments!\n");
+  }else if (argc == 2){
     printf("You have only one argument.\n");
   } else if (argc > 2 && argc < 4){
       printf("Here are your arguments:\n");
