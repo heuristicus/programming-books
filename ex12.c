e1f01c460cf58737ab334ac39f20b8641b2cd020
Michal Staniaszek <m.staniaszek@gmail.com>
2012-05-30 13:21:12 +0900

    up to ex12

---
+++ ex12.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  int i = 0;
+
+  if (argc == 2){
+    printf("You have only one argument.\n");
+  } else if (argc > 2 && argc < 4){
+      printf("Here are your arguments:\n");
+      for (i = 0; i < argc; i++){
+	printf("%s ", argv[i]);
+      }
+      printf("\n");
+  } else {
+    printf("You have too many arguments\n");
+  }
+  
+  return 0;
+}
+
