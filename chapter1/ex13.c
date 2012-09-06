2e21d7666ad20809077ba7f8ce8d3c3434865748
Michal Staniaszek <m.staniaszek@gmail.com>
2012-09-06 22:39:36 +0100

    up to ex22

---
+++ chapter1/ex13.c
@@ -0,0 +1,15 @@
+int main(int argc, char *argv[])
+{
+    int i;
+    
+    for (i = 0; i < 3; ++i){
+	
+    }
+    for (i = 0; i < 3; ++i){
+	;
+	;
+	;
+	
+    }
+    return 0;
+}
---
+++ chapter1/ex18.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    const int a = 0;
+    
+    a = 333;
+    printf("%d\n", a);
+    	
+    return 0;
+}
