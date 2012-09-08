0b8f624d6a49b28075c6bf6ad83d4a98131b5916
Michal Staniaszek <m.staniaszek@gmail.com>
2012-09-08 22:48:44 +0100

    last exercise of chapter 2

---
+++ chapter2/ex28-9-30.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int\
+ argc, char *argv[])
+{
+    printf("putting a long string in here so that we can check wheth\
+er c actually allows string constants to be written on two lines\n");
+    return 0;
+}
