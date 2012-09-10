dc3cbcffbd765d848ef3aea693bcbbc30ce7a8aa
Michal Staniaszek <m.staniaszek@gmail.com>
2012-09-10 23:33:06 +0100

    up to 22 (what the hell is 22 about)

---
+++ chapter4/ex19.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    for (; printf("TRUE "); printf("FOREVER\n")){
+	;
+    }
+    return 0;
+}
---
+++ chapter4/ex21-3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int i, error;
+    
+    do {
+	printf("Input a number between 1 and 3 (inclusive)\n");
+	scanf("%d", &i);
+	
+    } while (error = (i < 1 && i > 3));
+    
+    if (i == 1)
+	printf("statement 1 executed\n");
+    else if (i == 2)
+	printf("statement 2 executed\n");
+    else
+	printf("statement 3 executed\n");
+    
+    return 0;
+}
