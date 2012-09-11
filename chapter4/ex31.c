9f9c7b946dff34a0f3a93713b4299eb267dc56a0
Michal Staniaszek <m.staniaszek@gmail.com>
2012-09-11 14:58:06 +0100

    up to 33

--- chapter4/ex31.c
+++ chapter4/ex31.c
@@ -2,17 +2,28 @@
 
 int main(int argc, char *argv[])
 {
-    double a, b, c, d;
+    double a, b, c, d, r1, r2;
     int error;
     
-    printf("Enter three values. The roots of a polynomial with those coefficients will be computed.\n");
+    printf("Enter three values.\n");
     while ((error = scanf("%lf%lf%lf", &a, &b, &c)) == 3){
-	if (a == 0)
-	    handle_degenerate(a, b, c);
-	else
-	    handle_general(a, b, c);
-	printf("Enter three integers.\n");
-    }
+	d = b * b - 4.0 * a * c;
+	if (d == 0){
+	    if (a != 0.0)
+	        r1 = r2 = -b / (2.0 * a);
+	    else 
+		printf("degenerate case\n");
+	} else if (d > 0.0){
+	    if (a != 0.0){
+		r1 = (-b + sqrt(d)) / (2.0 * a);
+		r2 = (-b - sqrt(d)) / (2.0 * a);
+	    } else {
+		printf("degenerate case\n");
+	    }
+	} else {
+	    printf("imaginary roots!\n");
+	}
+    
 
     return 0;
 }
