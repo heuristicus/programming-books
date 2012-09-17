01882182fe7fb119bb9c198ea91f4ff578b033a0
Michal Staniaszek <m.staniaszek@gmail.com>
2012-09-17 23:18:53 +0100

    more fixes to ex29

--- chapter6/ex29.c
+++ chapter6/ex29.c
@@ -2,60 +2,87 @@
 #include <string.h>
 #include <stdlib.h>
 
+
 #define MAX_IN 500
 #define MAX_STR_LEN 50
 #define DICT_SIZE 100
 
-char *foreign[DICT_SIZE];
-char *english[DICT_SIZE];
-
-void read_dict(char *filename, char *arr[]);
-
+int read_file(char *filename, char *arr[], int size);
+void dealloc(char *arr[], int size);
+void str_spc_split(char *str, char *arr[]);
 
 int main(int argc, char *argv[])
 {
-    if (argc != 3){
-	printf("usage: ex29 english_words foreign_words\n");
+    if (argc != 4){
+	printf("usage: ex29 english_words foreign_words replace_text\n");
 	exit(1);
     }
 	
-    extern char *foreign[DICT_SIZE], *english[DICT_SIZE];
+    char *foreign[DICT_SIZE], *english[DICT_SIZE], *words[MAX_IN];
+    int i;
     
-    read_dict(argv[1], english);
-    read_dict(argv[2], foreign);
-    
-    int i = 0;
+    char *tmp = "thake that, koward! you are unpermitted in this place!";
+    str_spc_split(tmp, words);
+    for (i = 0; i < MAX_IN; ++i)
+	printf("%s\n", words[i]);
 
-    for (; i < DICT_SIZE; ++i){
-//	printf("eng: %s, for %s\n", english[i], foreign[i]);
-    }
+    return 0;
+    
+    read_file(argv[1], english, DICT_SIZE);
+    read_file(argv[2], foreign, DICT_SIZE);
+    read_file(argv[3], words, MAX_IN);
+    
 
-    char *words[MAX_IN];
-    char c;
     
-//    while((c = scanf("%s", &*words[i])) != EOF && ++i < MAX_IN);
-    
+    for (i = 0; i < MAX_IN; ++i){
+	printf("%s ", words[i]);
+    }    
     return 0;
 }
 
-void read_dict(char *filename, char *arr[])
+/* returns the last index that something was allocated to */
+int read_file(char *filename, char *arr[], int size)
 {
     FILE *fp = fopen(filename, "r");
     char tmp[MAX_STR_LEN], *tmp2;
     int i;
     
-    for (i = 0; i < DICT_SIZE; ++i){
+    for (i = 0; i < size; ++i){
 	    
 	if (fgets(tmp, MAX_STR_LEN, fp) == NULL)
-	    printf("Error while reading from file %s.\n", filename);
-	printf("%s", tmp);
+	    return i;
+		
 	arr[i] = calloc(strlen(tmp) + 1, sizeof(char));
+
 	if ((tmp2 = strchr(tmp, '\n')) != NULL)
 	    *tmp2 = '\0';
-	printf("%s\n", tmp);
-	arr[i] = tmp;
-	printf("\n");
+	strcpy(arr[i], tmp);
     }
     
-    
+    return size;
+}
+
+void str_spc_split(char *str, char *arr[])
+{
+    register char *wrd_strt = str, *cur_ch = str;
+    int i, len = strlen(str), size;
+
+    for (i = 0; i < len; ++i, *cur_ch++){
+	if (isblank(*cur_ch) || ispunct(*cur_ch)){
+	    size = cur_ch - wrd_strt + 1; // Size of the current word, plus one space for terminator
+	    
+	    printf("size of word %d\n", size);
+	}
+    }
+}
+
+/* frees all allocated memory in the array */
+void dealloc(char *arr[], int size)
+{
+    int i;
+        
+    for (i = 0; i < size; ++i){
+	if (arr[i] != NULL)
+	    free(arr[i]);
+    } 
 }
